fc84739a72ab9b026efcaeab0a140439a9471140
Muj-04 <mojahedxx123@gmail.com>
2026-07-13 20:32:07 +0300

    Fix Cloudflare build for Capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,23 @@
+const config = {
+  appId: 'com.studysync.app',
+  appName: 'StudySync',
+  webDir: 'out',
+  server: {
+    url: 'http://10.0.2.2:3000',
+    cleartext: true,
+    androidScheme: 'https',
+  },
+  android: {
+    overrideUserAgent: 'StudySync/1.0 Android',
+    captureInput: true,
+    webContentsDebuggingEnabled: true,
+  },
+  plugins: {
+    Keyboard: {
+      resize: 'body',
+      resizeOnFullScreen: true,
+    },
+  },
+};
+
+export default config;
